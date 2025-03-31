f639789a83041e483dcb51be36ed790f65a219ca
kulagin <alex_kulagin@samgk.ru>
2025-03-31 09:37:42 +0400

    Fix

--- Models/SupportiveFields.cs
+++ Models/SupportiveFields.cs
@@ -1,12 +1,12 @@
 namespace BlazorApp3.Models;
 
-public static class SupportiveFields
+public class SupportiveFields
 {
-    public static string SupAuthor { get; set; }
-    public static string SupMessage { get; set; }
-    public static int SupStarsCount { get; set; }
+    public string SupAuthor { get; set; }
+    public string SupMessage { get; set; }
+    public int SupStarsCount { get; set; }
     
-    public static void SetValue(int num)
+    public void SetValue(int num)
     {
         SupStarsCount = num;
     }
